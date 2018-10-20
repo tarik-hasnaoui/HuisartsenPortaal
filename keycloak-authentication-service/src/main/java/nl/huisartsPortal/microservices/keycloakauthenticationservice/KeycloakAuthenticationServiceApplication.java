692311b7ae90ee6390e43f36b052ee208d9a0447
Tarik Hasnaoui <tarik.hasnaoui@hva.nl>
2018-10-20 17:12:51 +0200

    Initial keycloak-authentication-service microservice

---
+++ keycloak-authentication-service/src/main/java/nl/huisartsPortal/microservices/keycloakauthenticationservice/KeycloakAuthenticationServiceApplication.java
@@ -0,0 +1,12 @@
+package nl.huisartsPortal.microservices.keycloakauthenticationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloakAuthenticationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeycloakAuthenticationServiceApplication.class, args);
+	}
+}
