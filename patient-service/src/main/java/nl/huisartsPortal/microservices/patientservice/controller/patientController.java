1a4eb0edf42c003b91b7d8f0b1807271ea5bb4cd
Tarik Hasnaoui <tarik.hasnaoui@hva.nl>
2018-10-20 20:04:27 +0200

    add filter to api gateway

--- patient-service/src/main/java/nl/huisartsPortal/microservices/patientservice/controller/patientController.java
+++ patient-service/src/main/java/nl/huisartsPortal/microservices/patientservice/controller/patientController.java
@@ -2,11 +2,13 @@ package nl.huisartsPortal.microservices.patientservice.controller;
 
 import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.ServletException;
 
 @RestController
+@RequestMapping("user")
 public class patientController {
 
         @GetMapping("/")
