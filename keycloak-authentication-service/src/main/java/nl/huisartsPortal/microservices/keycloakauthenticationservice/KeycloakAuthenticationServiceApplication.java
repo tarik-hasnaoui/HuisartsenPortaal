ce894e5cdfadeb16a9b950ccdf35a85d630fcffc
Tarik Hasnaoui <tarik.hasnaoui@hva.nl>
2018-11-18 21:45:27 +0100

    code cleaned

--- keycloak-authentication-service/src/main/java/nl/huisartsPortal/microservices/keycloakauthenticationservice/KeycloakAuthenticationServiceApplication.java
+++ keycloak-authentication-service/src/main/java/nl/huisartsPortal/microservices/keycloakauthenticationservice/KeycloakAuthenticationServiceApplication.java
@@ -8,7 +8,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 public class KeycloakAuthenticationServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(KeycloakAuthenticationServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(KeycloakAuthenticationServiceApplication.class, args);
+    }
 }
