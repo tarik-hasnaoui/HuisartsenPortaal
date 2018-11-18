d00eae8af7966f98aea0357ba1215868b48eddfa
Tarik Hasnaoui <tarik.hasnaoui@hva.nl>
2018-11-18 20:44:50 +0100

    data access object added to entities

--- prescription/src/main/java/nl/huisartsPortal/prescription/repository/DoctorDao.java
+++ prescription/src/main/java/nl/huisartsPortal/prescription/repository/DoctorDao.java
@@ -3,9 +3,6 @@ package nl.huisartsPortal.prescription.repository;
 import nl.huisartsPortal.prescription.model.Doctor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-/**
- * Created by tarik on 5-11-2018.
- */
 public interface DoctorDao extends JpaRepository<Doctor, Long> {
-    Doctor findByPolisNumber(String polisNumber);
+    Doctor findByBsnNumber(String bsnNumber);
 }
--- prescription/src/main/java/nl/huisartsPortal/prescription/repository/PatientDao.java
+++ prescription/src/main/java/nl/huisartsPortal/prescription/repository/PatientDao.java
@@ -3,11 +3,6 @@ package nl.huisartsPortal.prescription.repository;
 import nl.huisartsPortal.prescription.model.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-/**
- * Created by tarik on 5-11-2018.
- */
 public interface PatientDao extends JpaRepository<Patient, Long> {
-
-    Patient findByFirstName(String voorName);
-    Patient getPatientById(Long id);
+    Patient getPatientByBsnNumber(String bsnNumber);
 }
